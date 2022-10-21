77f1bd9e36a725c43dfe7d5014b932e56fb8a6cc
jasonanca <jason_anca@hotmail.com>
2022-10-21 14:42:37 +0100

    Exercise - Show favourite icon on Movie Details

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,9 +5,13 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const MovieHeader = (props) => {
   const movie = props.movie;
+  const favouriteMovies = JSON.parse(localStorage.getItem("favourites")); 
+  const idMovie = favouriteMovies.map((map) => map.id)
 
   return (
     <Paper 
@@ -23,6 +27,14 @@ const MovieHeader = (props) => {
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
+
+      {
+        idMovie.includes(props.movie.id) ? (
+          <Avatar sx={{backgroundColor: 'red'}}>
+            <FavoriteIcon />
+          </Avatar>
+        ) : null
+      }
 
       <Typography variant="h4" component="h3">
         {movie.title}
