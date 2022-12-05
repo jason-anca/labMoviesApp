fab5bf8e338d192233745b071a80ccfe457ae204
Jason Anca <jason_anca@hotmail.com>
2022-12-05 14:15:29 +0000

    test commit

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -1,4 +1,5 @@
 //Movie related gets
+//test
 export const getMovies = () => {
   return fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
